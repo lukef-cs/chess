5655353fa2028f947a0e5a6d91f7d921acd5c5f0
Luke Fairbanks <lukedfairbanks@gmail.com>
2025-09-10 09:32:02 -0600

    add queen and rook (same code for directions)

--- shared/src/main/java/chess/movecalculators/RookMoveCalculator.java
+++ shared/src/main/java/chess/movecalculators/RookMoveCalculator.java
@@ -4,6 +4,7 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -15,7 +16,31 @@ public class RookMoveCalculator {
     };
 
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
-        // Placeholder for King move calculation logic
-        return List.of();
+        int startRow = position.getRow();
+        int startCol = position.getColumn();
+        List<ChessMove> moves = new ArrayList<>();
+        for (int[] direction : ROOK_DIRECTIONS) {
+            int dRow = direction[0];
+            int dCol = direction[1];
+            int currentRow = startRow + dRow;
+            int currentCol = startCol + dCol;
+
+            while (currentRow > 0 && currentRow <= 8 && currentCol > 0 && currentCol <= 8) {
+                ChessPosition newPosition = new ChessPosition(currentRow, currentCol);
+                // detect if we've hit a piece
+                if (board.getPiece(newPosition) != null) {
+                    // If it's an opponent's piece, we can capture it
+                    if (board.getPiece(newPosition).getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        moves.add(new ChessMove(position, newPosition, null));
+                    }
+                    break; // Stop moving in this direction
+                }
+
+                moves.add(new ChessMove(position, newPosition, null));
+                currentRow += dRow;
+                currentCol += dCol;
+            }
+        }
+        return moves;
     }
 }
