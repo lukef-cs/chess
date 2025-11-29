5ae4272bfa6fe82dd623e9fd3581a1c7fb47c441
Luke Fairbanks <lukedfairbanks@gmail.com>
2025-11-29 14:24:44 -0700

    connection

---
+++ server/src/main/java/websocket/Connection.java
@@ -0,0 +1,20 @@
+package websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import java.io.IOException;
+
+public class Connection {
+    public String visitorName;
+    public Session session;
+    public Integer gameId;
+
+    public Connection(String visitorName, Session session, Integer gameId) {
+        this.visitorName = visitorName;
+        this.session = session;
+        this.gameId = gameId;
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
+}
