be64832fd2c0129803fa4593123de6bbfd7f4429
Luke Fairbanks <lukedfairbanks@gmail.com>
2025-11-30 20:33:26 -0700

    websocket facade

---
+++ client/src/main/java/client/websocket/ServerMessageObserver.java
@@ -0,0 +1,6 @@
+package client.websocket;
+import websocket.messages.ServerMessage;;
+
+public interface ServerMessageObserver {
+    void notify(ServerMessage message);
+}
---
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -0,0 +1,91 @@
+package client.websocket;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.google.gson.Gson;
+
+import chess.ChessMove;
+import jakarta.websocket.*;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
+
+public class WebSocketFacade extends Endpoint {
+
+    Session session;
+    ServerMessageObserver notificationHandler;
+
+    public WebSocketFacade(String url, ServerMessageObserver notificationHandler) throws Exception {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                    switch (serverMessage.getServerMessageType()) {
+                        case LOAD_GAME -> notificationHandler.notify(new Gson().fromJson(message, LoadGameMessage.class));
+                        case ERROR -> notificationHandler.notify(new Gson().fromJson(message, ErrorMessage.class));
+                        case NOTIFICATION -> notificationHandler.notify(new Gson().fromJson(message, NotificationMessage.class));
+                    }
+                }
+            });
+
+
+        } catch (DeploymentException | IOException | URISyntaxException e) {
+            throw new Exception("500: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig config) {
+        this.session = session;
+    }
+
+    public void joinGame(String authToken, Integer gameID) throws Exception {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException e) {
+            throw new Exception("500: " + e.getMessage());
+        }
+    }
+
+    public void makeMove(String authToken, Integer gameID, ChessMove move) throws Exception {
+        try {
+            MakeMoveCommand command = new MakeMoveCommand(move, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException e) {
+            throw new Exception("500: " + e.getMessage());
+        }
+    }
+
+    public void leave(String authToken, int gameID) throws Exception {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.LEAVE, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+            this.session.close();
+        } catch (IOException e) {
+            throw new Exception("500: " + e.getMessage());
+        }
+    }
+
+    public void resign(String authToken, int gameID) throws Exception {
+        try {
+            UserGameCommand command = new UserGameCommand(UserGameCommand.CommandType.RESIGN, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException e) {
+            throw new Exception("500: " + e.getMessage());
+        }
+    }
+}
